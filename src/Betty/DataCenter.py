dcecc0972262aa04c547c7ffe8fc74e5c6266da3
Sam Badger <smalbadger@email.arizonae.edu>
2018-01-13 00:08:07 -0700

    moved printing functionality from bot class to data_center class

--- src/Betty/DataCenter.py
+++ src/Betty/DataCenter.py
@@ -84,3 +84,103 @@ class DataCenter():
         
         pass
 
+    """
+    Don't worry about this code for now. It will all change later.
+    
+    
+    #This method just generates a portfolio donut chart using plotly. 
+    #Currently, the picture gets put on their website
+    def create_portfolio(self):
+        prices = {}
+        for i in self._robot.client().get_products():
+            p_id = i["id"]
+            if "USD" in p_id:
+                prices[p_id] = float(self.client().get_product_ticker(product_id=p_id)["price"])
+
+        USD, BTC_amount, BCH_amount, ETH_amount, LTC_amount = self.get_balances(all_currencies=True)
+        BTC_worth, BCH_worth, ETH_worth, LTC_worth = (BTC_amount * prices["BTC-USD"]), (BCH_amount * prices["BCH-USD"]), (ETH_amount * prices["ETH-USD"]), (LTC_amount * prices["LTC-USD"]),
+
+        net_worth = USD + BTC_worth + BCH_worth + ETH_worth + LTC_worth
+        net_worth = round(net_worth, 2)
+        title = "GDAX\n${}".format(net_worth)
+
+        plotly.tools.set_credentials_file(username='TradeBotTeam', api_key='SJTSXTDYHHtwLyul8olP')
+        fig = {
+            "data": [{
+                 "values": [USD, BTC_worth, BCH_worth, ETH_worth, LTC_worth],
+                 "labels": ["USD", "BTC", "BCH", "ETH", "LTC"],
+                 "domain": {"x": [0, 1]},
+                 "name"  : "Portfolio",
+                 "hoverinfo": "label+percent+name",
+                 "hole": .5,
+                 "type": "pie"
+             }],
+             "layout": {
+                 "title": "Portfolio",
+                 "annotations": [{
+                     "font": { "size": 20 },
+                     "showarrow": False,
+                     "text": title
+                 }]
+             }
+        }
+
+        pie_chart = py.plot(fig, filename="GDAX_porfolio_pie_chart")
+        return pie_chart
+
+    #This method uses the client object to print a list of all available crypto prices
+    def print_current_prices(self):
+        for i in self._robot.client().get_products():
+            p_id = i["id"]
+            if "USD" in p_id:
+                print(p_id + " : " + str(self.client().get_product_ticker(product_id=p_id)["price"]))
+
+
+    def plot_session(self): 
+            
+        base_price = self._crypto_history[self._robot.currency()][0]['price']
+        base_portfolio_value = self._portfolio_history[0]["total"]
+        
+        x_axis = []
+        portfolio_value_at_trading = self._portfolio_at_trading[:] 
+        prices_at_trading = self._prices_at_trading[:]
+        
+        difference = []
+        for i in range(len(self._prices_at_trading)):
+            x_axis.append(i)
+            portfolio_value_at_trading[i] = ((self._portfolio_at_trading[i]["value"] / base_portfolio_value) -1 ) * 100
+            prices_at_trading[i] = ((self._prices_at_trading[i]["value"] / base_price) -1 )* 100
+            
+            difference.append(portfolio_value_at_trading[i] - prices_at_trading[i])
+            
+            #if self._portfolio_value_at_trading[i]["state"] != 0:
+            #    portfolio_color_at_trading[i] = self._portfolio_value_at_trading[i]["state"] + 1
+            #else:
+            #    portfolio_color_at_trading[i] = self._portfolio_value_at_trading[i]["state"]
+            
+            
+        trace1 = go.Scatter(
+            x = x_axis,
+            y = prices_at_trading,
+            name = 'product',
+            connectgaps = True
+        )
+        trace2 = go.Scatter(
+            x = x_axis,
+            y = portfolio_value_at_trading,
+            name = 'portfolio',
+            connectgaps = True
+        )
+        trace3 = go.Scatter(
+            x = x_axis,
+            y = difference,
+            name = 'difference',
+            connectgaps = True
+        )
+
+        data = [trace1, trace2, trace3]
+
+        fig = dict(data=data)
+        py.plot(fig, filename=(self.name()+"_results"))
+        """
+        
